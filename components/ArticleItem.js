4811dc0f1d37da7ea1b41974362c330c161c3911
praconfi <praconfi@gmail.com>
2022-02-10 11:07:03 +0900

    update code simpler

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,12 +1,11 @@
 import Link from 'next/link'
 import articleStyles from '../styles/Article.module.css'
-
 import React from 'react'
 
 const ArticleItem = ({article}) => {
   return (
     <div>
-      <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <Link href={`/article/${article.id}`}>
         <a>
             <div className={articleStyles.card}>
                 <h3>{article.title} &rarr;</h3>
