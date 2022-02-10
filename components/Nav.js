b647d8c85f5832b19bf2c823d7e152f5d9737679
praconfi <praconfi@gmail.com>
2022-02-10 11:04:52 +0900

    add menus and auth actions

--- components/Nav.js
+++ components/Nav.js
@@ -5,20 +5,33 @@ import Link from 'next/link'
 const Nav = () => {
   return (
     <div className={navStyle.nav}>
-        <ul>
-            <li>
-                <Link href='/' >Home</Link>
-            </li>
-            <li>
-                <Link href='/about' >About</Link>
-            </li>
-            <li>
-                <Link href='/login' >Login</Link>
-            </li>
-            <li>
-                <Link href='/grid' >Grid</Link>
-            </li>
-        </ul>
+        <a className={navStyle.logo}>
+            <h2>APP_NAME</h2>
+        </a>
+        <div className={navStyle.nav__menu}>
+            <ul>
+                <li>
+                    <Link href='/' >Home</Link>
+                </li>
+                <li>
+                    <Link href='/about' >About</Link>
+                </li>
+                
+                <li>
+                    <Link href='/grid' >그리드</Link>
+                </li>
+            </ul>
+        </div>
+        <div className={navStyle.nav__auth_actions}>
+            <ul>
+                <li>
+                    <a>회원가입</a>
+                </li>
+                <li>
+                    <Link href='/login' >로그인</Link>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
