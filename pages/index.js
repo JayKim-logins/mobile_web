0e27c4ae6df450076b30e159ba703b1cefb4b6d4
praconfi <praconfi@gmail.com>
2022-02-10 18:08:17 +0900

    add scrollTo- nav height

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,44 @@
 import Head from 'next/head'
-import ArticleList from '../components/ArticleList'
-import {server} from '../config'
+import StrengthSection from '../components/StrengthSection'
 import homeStyle from '../styles/Home.module.css'
+import LandingScreenSection from '../components/LandingScreenSection'
+import ServiceSection from '../components/ServiceSection'
 
 export default function Home({ articles }) {
   
+  
+  function scrollToTargetAdjusted(selector){
+    var element = document.querySelector(selector);
+    var headerOffset = 80;
+    var elementPosition = element.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+  
+    window.scrollTo({
+         top: offsetPosition,
+         behavior: "smooth"
+    });
+}
+
+
   return (
+
     <main className={homeStyle.wrap}>
       {/* 메타태그 추가 */}
       <Head> 
         <title>mobile web</title>
       </Head>
-      <div className={homeStyle.background}>
-      </div>
-      <div className={homeStyle.arrow}>
-        
-          <span style={{width:"30px"}} className={homeStyle.arrow__span}><canvas width="12" height="8" style={{width:"30px"}} className=""></canvas><svg className="" style={{position:"absolute",top:"0px",left:"0px",width:"100%",height:"auto"}} width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.41 0.590088L6 5.17009L10.59 0.590088L12 2.00009L6 8.00009L0 2.00009L1.41 0.590088Z" fill="#ffffff"></path></svg></span>
-        
-      </div>
+
+      <LandingScreenSection scrollToTargetAdjusted={scrollToTargetAdjusted} />
+      <section>
+      
+      </section>
+      <ServiceSection />
+      <StrengthSection id="a"/>
+      <StrengthSection id="b"/>
+      <StrengthSection id="c"/>
+      <StrengthSection id="d"/>
+      <StrengthSection id="e"/>
+      
     </main>
   )
 }
