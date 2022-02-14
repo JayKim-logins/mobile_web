0ae10bb6d6d80f2a3dc333c7a08565a6557c8f0d
praconfi <praconfi@gmail.com>
2022-02-14 14:54:32 +0900

    add AUIGrid

---
+++ public/AUIGrid/AUIGridLicense.js
@@ -0,0 +1,8 @@
+/**
+ * AUIGrid 3.0.10 License
+ * License Type : Enterprise License
+ * Product ID : AUIGrid-EN-14933-592322-160122
+ * Authorized Domain : *.tlogin.net
+ */
+ /* eslint-disable */
+AUIGridLicense = "eyJjdCI6InN0QnFpSEZuYXpKa2pzM3BFZU44cG4ybTdsT0ZCYXRFSGhNS2JYRTVtUWF6aGJOajVEdXp3S0hCMXVWS1FrSTdOb2JtNklpbkJVTlE4N0hUMnIvZ25vNXFkZCtMY25VbUM4ZmJTYjB4djJjPSIsIml2IjoiYmRkZTU2ZjJkNzRmOGQ2NDhjN2U5MDFiODc4OGIzODYiLCJzIjoiODU3ZjJiMjg0YzZmMzc3MiJ9";
